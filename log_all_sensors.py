9de83ee908e336aeb3280c4cda26208d5940ce8f
James Bithell <Jbithell@users.noreply.github.com>
2017-02-23 22:18:15 +0000

    remove direction

--- log_all_sensors.py
+++ log_all_sensors.py
@@ -6,12 +6,12 @@ import interrupt_client
 import wind_direction
 
 FREQUENCY = 60 * 5  #Record data to thingspeak at this frequency
-wind_dir = wind_direction.wind_direction(adc_channel = 7, config_file="wind_direction.json")
+#wind_dir = wind_direction.wind_direction(adc_channel = 7, config_file="wind_direction.json")
 interrupts = interrupt_client.interrupt_client(port = 49501)
 if __name__ == "__main__":
 	while True:
-		wind_average = wind_dir.get_value(10) #ten seconds
-		print(wind_average)
+		#wind_average = wind_dir.get_value(10) #ten seconds
+		#print(wind_average)
 		print(interrupts.get_wind())
 		print(interrupts.get_wind_gust())
 		interrupts.reset()

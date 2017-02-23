c0d7a9dc6846f27653e27081390c6d24b4f44213
James Bithell <Jbithell@users.noreply.github.com>
2017-02-23 22:21:03 +0000

    Add comments

--- log_all_sensors.py
+++ log_all_sensors.py
@@ -5,15 +5,15 @@ import time
 import interrupt_client
 import wind_direction
 
-FREQUENCY = 5  #Record data to thingspeak at this frequency
+FREQUENCY = 2  #Record data to thingspeak at this frequency
 #wind_dir = wind_direction.wind_direction(adc_channel = 7, config_file="wind_direction.json")
 interrupts = interrupt_client.interrupt_client(port = 49501)
 if __name__ == "__main__":
 	while True:
 		#wind_average = wind_dir.get_value(10) #ten seconds
 		#print(wind_average)
-		print(interrupts.get_wind())
-		print(interrupts.get_wind_gust())
+		print("Speed " + str(interrupts.get_wind()))
+		print("Gusting " + str(interrupts.get_wind_gust()))
 		interrupts.reset()
 		time.sleep(FREQUENCY)
 		

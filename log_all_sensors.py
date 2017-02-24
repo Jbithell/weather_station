9f9f089f12db3c4e2beeec5b6dab9cd97a7fab3e
James Bithell <Jbithell@users.noreply.github.com>
2017-02-24 20:55:32 +0000

    Remove extra text

--- log_all_sensors.py
+++ log_all_sensors.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 		#wind_average = wind_dir.get_value(10) #ten seconds
 		#print(wind_average)
 		if (interrupts.get_wind() >0):
-		    print("********** Speed " + str(interrupts.get_wind()))
+		    print(interrupts.get_wind())
 		    interrupts.reset()
 		#interrupts.get_wind_gust() gives you the highest value in last 5 seconds - assuming you don't reset interrupts
 		

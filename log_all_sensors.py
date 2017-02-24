f0b27368262d5a4c92e2be82e22822e1bbfec889
James Bithell <Jbithell@users.noreply.github.com>
2017-02-24 20:52:47 +0000

    Remove gust data

--- log_all_sensors.py
+++ log_all_sensors.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
 		#wind_average = wind_dir.get_value(10) #ten seconds
 		#print(wind_average)
 		if (interrupts.get_wind() >0):
-		    print("Speed " + str(interrupts.get_wind()))
-		    print("Gusting " + str(interrupts.get_wind_gust()))
+		    print("********** Speed " + str(interrupts.get_wind()))
 		    interrupts.reset()
+		#interrupts.get_wind_gust() gives you the highest value in last 5 seconds - assuming you don't reset interrupts
+		
 	

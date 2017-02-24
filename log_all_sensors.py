e36103bc5336bb3bda283c2cab8e12ce413f8a8c
James Bithell <Jbithell@users.noreply.github.com>
2017-02-24 20:46:46 +0000

    Only show data if it isn't zero

--- log_all_sensors.py
+++ log_all_sensors.py
@@ -5,11 +5,9 @@ import time
 import interrupt_client
 import wind_direction
 
-FREQUENCY = 2  #Record data to thingspeak at this frequency
 #wind_dir = wind_direction.wind_direction(adc_channel = 7, config_file="wind_direction.json")
 interrupts = interrupt_client.interrupt_client(port = 49501)
 if __name__ == "__main__":
-	latestreading = 0
 	while True:
 		#wind_average = wind_dir.get_value(10) #ten seconds
 		#print(wind_average)
@@ -17,8 +15,4 @@ if __name__ == "__main__":
 		    print("Speed " + str(interrupts.get_wind()))
 		    print("Gusting " + str(interrupts.get_wind_gust()))
 		    interrupts.reset()
-		    print(time.time()-latestreading)
-		    latestreading = time.time()
-		#time.sleep(FREQUENCY)
-		
 	

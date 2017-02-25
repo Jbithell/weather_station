44897ba579f36a1f586ed7dd176cca63fd4a6721
James Bithell <Jbithell@users.noreply.github.com>
2017-02-25 16:53:56 +0000

    Add a new main

---
+++ src/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#import interrupt_client, MCP342X, wind_direction, HTU21D, bmp085, tgs2600, ds18b20_therm
+import time
+import interrupt_client
+import wind_direction
+
+#wind_dir = wind_direction.wind_direction(adc_channel = 7, config_file="wind_direction.json")
+interrupts = interrupt_client.interrupt_client(port = 49501)
+if __name__ == "__main__":
+	while True:
+		#wind_average = wind_dir.get_value(10) #ten seconds
+		#print(wind_average)
+		if (interrupts.get_wind() >0):
+		    print(interrupts.get_wind()*0.277778) #Speed in M/s (*1.9438460492 for Knots)
+		    interrupts.reset()
+		#interrupts.get_wind_gust() gives you the highest value in last 5 seconds - assuming you don't reset interrupts
+		
+	

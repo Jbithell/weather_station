38106ce39e6e68feece7e0c7e6192dce08e1a232
James Bithell <hi@jbithell.com>
2017-03-04 18:47:06 +0000

    Debug a pin

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import InterruptClient
 import WindDirection
 
 import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(21, GPIO.OUT)
 GPIO.output(21, GPIO.HIGH)
 time.sleep(1236549543542354368)

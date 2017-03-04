9ae0104da0480dab777c123dcf48b30960354fe6
James Bithell <hi@jbithell.com>
2017-03-04 21:38:35 +0000

    Check wind speed instead

--- src/main.py
+++ src/main.py
@@ -17,10 +17,10 @@ wind_dir = WindDirection.wind_direction(adc_channel = 7, config_file="wind_direc
 interrupts = InterruptClient.interrupt_client(port = 49501)
 if __name__ == "__main__":
 	while True:
-		wind_average = wind_dir.get_value(5) #Time to get average for in seconds
-		print(wind_average)
+		#wind_average = wind_dir.get_value(5) #Time to get average for in seconds
+		#print(wind_average)
 		if (interrupts.get_wind() >0):
-		    #print(interrupts.get_wind()*0.277778) #Speed in M/s (*1.9438460492 for Knots)
+		    print(interrupts.get_wind()*0.277778) #Speed in M/s (*1.9438460492 for Knots)
 		    interrupts.reset()
 		#interrupts.get_wind_gust() gives you the highest value in last 5 seconds - assuming you don't reset interrupts
 		

4eb96e4014200aac82d4f308d6e5da75399d4092
James Bithell <hi@jbithell.com>
2017-03-04 15:27:34 +0000

    Add in wind

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ import time
 import InterruptClient
 import WindDirection
 
-#wind_dir = WindDirection.wind_direction(adc_channel = 7, config_file="wind_direction.json")
+wind_dir = WindDirection.wind_direction(adc_channel = 7, config_file="wind_direction.json")
 interrupts = InterruptClient.interrupt_client(port = 49501)
 if __name__ == "__main__":
 	while True:
-		#wind_average = wind_dir.get_value(10) #ten seconds
-		#print(wind_average)
+		wind_average = wind_dir.get_value(2) #ten seconds
+		print(wind_average)
 		if (interrupts.get_wind() >0):
 		    print(interrupts.get_wind()*0.277778) #Speed in M/s (*1.9438460492 for Knots)
 		    interrupts.reset()

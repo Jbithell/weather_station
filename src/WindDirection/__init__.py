905411e71d8e1dc02b7da978aa29d35da4992161
James Bithell <hi@jbithell.com>
2017-03-04 20:40:26 +0000

    Another debug attempt

--- src/WindDirection/__init__.py
+++ src/WindDirection/__init__.py
@@ -111,13 +111,15 @@ class wind_direction(object):
 
         while time.time() - start_time <= length:
             adc_value = self.adc.value
-            print(adc_value)
-            direction = self.get_dir(adc_value)
-            if direction is not None:  # keep only good measurements
-                data.append(direction)
-            elif adc_value != 0:
-                print("Could not determine wind direction for ADC reading: %s" % adc_value)
-            time.sleep(1)
+            if (adc_value > 0):
+                print(adc_value)
+                time.sleep(1)
+            #direction = self.get_dir(adc_value)
+            #if direction is not None:  # keep only good measurements
+            #    data.append(direction)
+            #elif adc_value != 0:
+            #    print("Could not determine wind direction for ADC reading: %s" % adc_value)
+
         return self.get_average(data)
 
 if __name__ == "__main__":

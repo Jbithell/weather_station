f92235ae8d1a70d52b6c601fa9cf9b1160ef9a54
James Bithell <hi@jbithell.com>
2017-03-04 19:39:02 +0000

    Finish pin debug

--- src/main.py
+++ src/main.py
@@ -5,19 +5,20 @@ import time
 import InterruptClient
 import WindDirection
 
+'''
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(40, GPIO.OUT)
 GPIO.output(40, GPIO.HIGH)
 time.sleep(1236549543542354368)
-
+'''
 
 wind_dir = WindDirection.wind_direction(adc_channel = 7, config_file="wind_direction.json")
 interrupts = InterruptClient.interrupt_client(port = 49501)
 if __name__ == "__main__":
 	while True:
-		#wind_average = wind_dir.get_value(4) #ten seconds
-		#print(wind_average)
+		wind_average = wind_dir.get_value(4) #ten seconds
+		print(wind_average)
 		if (interrupts.get_wind() >0):
 		    print(interrupts.get_wind()*0.277778) #Speed in M/s (*1.9438460492 for Knots)
 		    interrupts.reset()

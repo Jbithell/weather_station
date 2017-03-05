0640a3b49bd7675cb0119a9757d317e3ae92a886
James Bithell <hi@jbithell.com>
2017-03-05 16:16:06 +0000

    Test LED

--- src/main.py
+++ src/main.py
@@ -5,22 +5,22 @@ import time
 import InterruptClient
 import WindDirection
 
-'''
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40, GPIO.OUT)
-GPIO.output(40, GPIO.HIGH)
-time.sleep(1236549543542354368)
-'''
 
-wind_dir = WindDirection.wind_direction(adc_channel = 7, config_file="wind_direction.json")
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+GPIO.output(26, GPIO.HIGH)
+time.sleep(1236549543542354368)
+
+
+wind_dir = WindDirection.wind_direction(adc_channel = 1, config_file="wind_direction.json")
 interrupts = InterruptClient.interrupt_client(port = 49501)
 if __name__ == "__main__":
 	while True:
 		#wind_average = wind_dir.get_value(5) #Time to get average for in seconds
 		#print(wind_average)
 		if (interrupts.get_wind() >0):
-		    print(interrupts.get_wind()*0.277778) #Speed in M/s (*1.9438460492 for Knots)
+		    print(interrupts.cd.get_wind()*0.277778) #Speed in M/s (*1.9438460492 for Knots)
 		    interrupts.reset()
 		#interrupts.get_wind_gust() gives you the highest value in last 5 seconds - assuming you don't reset interrupts
 		

ff5b9430b0368f6ff75f67f866392c2e5177d488
James Bithell <hi@jbithell.com>
2017-03-05 16:43:10 +0000

    Make led flash proportional to wind speed

--- src/main.py
+++ src/main.py
@@ -17,15 +17,17 @@ GPIO.setup(26, GPIO.OUT) #Setup LED
 GPIO.output(26,0) #Setup LED
 
 if __name__ == "__main__":
+    lastwind = 0
     while True:
         GPIO.output(26, 1)
-        time.sleep(0.5)
+        time.sleep(1/lastwind)
         GPIO.output(26, 0)
-        time.sleep(0.5)
+        time.sleep(1/lastwind)
         #wind_average = wind_dir.get_value(5) #Time to get average for in seconds
         #print(wind_average)
         if (interrupts.get_wind() >0):
             print(interrupts.get_wind()*0.277778) #Speed in M/s (*1.9438460492 for Knots)
+            lastwind = (interrupts.get_wind()*0.277778)
             interrupts.reset()
             #interrupts.get_wind_gust() gives you the highest value in last 5 seconds - assuming you don't reset interrupts
 
